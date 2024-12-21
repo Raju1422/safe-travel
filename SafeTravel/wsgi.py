5187731c75d6d8cca89685d76203e42d08a03d34
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2024-12-21 11:16:08 +0530

    asgi changes

--- SafeTravel/wsgi.py
+++ SafeTravel/wsgi.py
@@ -1,27 +1,16 @@
 """
-ASGI config for SafeTravel project.
+WSGI config for SafeTravel project.
 
-It exposes the ASGI callable as a module-level variable named `application`.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import core.routing
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SafeTravel.settings')
 
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            core.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_wsgi_application()
